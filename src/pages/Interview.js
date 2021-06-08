5354600b471ecd43402966bcdc0e96c3bb6977fd
Jagan <jaganraajan@gmail.com>
2021-06-08 08:19:35 -0400

    added hero section for interview.js

--- src/pages/Interview.js
+++ src/pages/Interview.js
@@ -1,41 +1,52 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import styled from "styled-components";
 import tw from "twin.macro";
-import styled, { css } from "styled-components/macro"; //eslint-disable-line
-import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { css } from "styled-components/macro"; //eslint-disable-line
 import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
 import { SectionDescription } from "components/misc/Typography.js";
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
 import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
+import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-7.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-8.svg";
+import Hero from "components/hero/TwoColumnWithFeaturesAndTestimonial";
+const Subheading = tw(SubheadingBase)`mb-4 text-center`;
+const Heading = tw(SectionHeading)`w-full`;
+const Description = tw(SectionDescription)`w-full text-center`;
 
-const PrimaryBackgroundContainer = tw(Container)`-mx-8 px-8 bg-primary-900 text-gray-100`;
+const Column = tw.div`flex flex-col items-center`;
+const HeaderContent = tw.div``;
 
-const HeadingContainer = tw.div``;
-const Subheading = tw(SubheadingBase)`text-center text-gray-100 mb-4`;
-const Heading = tw(SectionHeading)``;
-const Description = tw(SectionDescription)`mx-auto text-center text-gray-300`;
-
-const FaqsContainer = tw.div`mt-10 sm:mt-16 w-full flex-1 lg:flex justify-between items-start max-w-screen-lg mx-auto`;
-const FaqsColumn = tw.div`w-full lg:max-w-lg lg:mr-12 last:mr-0`;
-const Faq = tw.div`select-none cursor-pointer border-b-2 border-primary-300 hover:border-primary-500 transition-colors duration-300 py-6`;
-const Question = tw.div`flex justify-between items-center`;
-const QuestionText = tw.div`text-sm sm:text-lg font-semibold tracking-wide`;
-const QuestionToggleIcon = styled(motion.span)`
+const FAQSContainer = tw.dl`mt-12 max-w-4xl relative`;
+const FAQ = tw.div` select-none mt-5 px-8 sm:px-10 py-5 sm:py-4 rounded-lg text-gray-800  bg-gray-200  transition duration-300`;
+const Question = tw.dt`flex justify-between items-center`;
+const QuestionText = tw.span`text-lg lg:text-xl font-semibold`;
+const QuestionToggleIcon = motion(styled.span`
   ${tw`ml-2 transition duration-300`}
   svg {
     ${tw`w-6 h-6`}
   }
+`);
+const Answer = (tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`);
+
+const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+  ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400`}
 `;
-const Answer = tw(motion.div)`hidden text-sm font-normal mt-4 text-gray-300`;
+const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
+  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-15 transform -translate-x-2/3 text-primary-500`}
+`;
+
+
 
 export default ({
-  subheading = "",
-  heading = "Frequently Asked Questions",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  subheading = "FAQS",
+  heading = "You have Questions ?",
+  description = "And we have got answers to all of them. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   faqs = [
     {
-      question: "Is lunch provided free of cost ?",
+      question: "Tell us about your journey from Linda-Marie Miller to Toastmaster Linda-Marie Miller, First Runner-up at World Championship of Public Speaking, 2020?",
       answer:
-        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system."
+        "I joined Toastmasters in the fall of 2019 and was asked to step in right away as the VP of Education.  Because I was an officer, I attended a TLI in December of 2019.  At that TLI, I attended a session delivered by a prior Finalist and it awakened in me a desire to think about competing in the contest because it sounded like a lot of fun.  I entered the contest that began in my home club in early 2020 and just kept winning at every level.  I never expected to find myself in the Semifinals and I was thrilled to have made it that far, knowing that there was no way a new Toastmaster that nobody had ever heard of would make it to the final stage. When they announced my name, I was shocked.     a     I was even more shocked that I was the 1st Runner-Up in The World Championship of Public Speaking. Since then, I have spoken to thousands of people all over the world and I am honored that I have a platform to make a difference for others."
     },
     {
       question: "Do you have 2 Bedroom suites ?",
@@ -51,77 +62,67 @@ export default ({
       question: "Where can I reach you for support ?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-      question: "What kind of SLA Guarantee do you provide ? ",
-      answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-      question: "Where are the servers located ?",
-      answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     }
   ]
 }) => {
-  const faqCol1 = [];
-  const faqCol2 = [];
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
+//   const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
 
-  const toggleQuestion = questionIndex => {
-    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
-    else setActiveQuestionIndex(questionIndex);
-  };
+//   const toggleQuestion = questionIndex => {
+//     if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
+//     else setActiveQuestionIndex(questionIndex);
+//   };
 
-  faqs.map((faq, index) => {
-    const renderedFaq = (
-      <Faq key={index} onClick={() => toggleQuestion(index)}>
-        <Question>
-          <QuestionText>{faq.question}</QuestionText>
-          <QuestionToggleIcon
-            variants={{
-              collapsed: { rotate: 0 },
-              open: { rotate: -180 }
-            }}
-            initial="collapsed"
-            animate={activeQuestionIndex === index ? "open" : "collapsed"}
-            transition={{ duration: 0.02, ease: [0.04, 0.62, 0.23, 0.98] }}
-          >
-            <ChevronDownIcon />
-          </QuestionToggleIcon>
-        </Question>
-        <Answer
-          variants={{
-            open: { opacity: 1, height: "auto", marginTop: "16px", display: "block" },
-            collapsed: { opacity: 0, height: 0, marginTop: "0px", display: "none" }
-          }}
-          initial="collapsed"
-          animate={activeQuestionIndex === index ? "open" : "collapsed"}
-          transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
-        >
-          {faq.answer}
-        </Answer>
-      </Faq>
-    );
-
-    if (index % 2 === 0) faqCol1.push(renderedFaq);
-    else faqCol2.push(renderedFaq);
-
-    return null;
-  });
   return (
-    <PrimaryBackgroundContainer>
+    <Container>
+      <Hero />
       <ContentWithPaddingXl>
-        <HeadingContainer>
-          {subheading && <Subheading>{subheading}</Subheading>}
-          <Heading>{heading}</Heading>
-          <Description>{description}</Description>
-        </HeadingContainer>
-        <FaqsContainer>
-          <FaqsColumn>{faqCol1}</FaqsColumn>
-          <FaqsColumn>{faqCol2}</FaqsColumn>
-        </FaqsContainer>
+        <Column>
+          <HeaderContent>
+            {subheading && <Subheading>{subheading}</Subheading>}
+            <Heading>{heading}</Heading>
+            {description && <Description>{description}</Description>}
+          </HeaderContent>
+          <FAQSContainer>
+            {faqs.map((faq, index) => (
+              <FAQ
+                key={index}
+                onClick={() => {
+                //   toggleQuestion(index);
+                }}
+                className="group"
+              >
+                <Question>
+                  <QuestionText>{faq.question}</QuestionText>
+                  {/* <QuestionToggleIcon
+                    variants={{
+                      collapsed: { rotate: 0 },
+                      open: { rotate: -180 }
+                    }}
+                    initial="collapsed"
+                    animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                    transition={{ duration: 0.02, ease: [0.04, 0.62, 0.23, 0.98] }}
+                  >
+                    <ChevronDownIcon />
+                  </QuestionToggleIcon> */}
+                </Question>
+                <Answer
+                  variants={{
+                    open: { opacity: 1, height: "auto", marginTop: "16px" },
+                    collapsed: { opacity: 0, height: 0, marginTop: "0px" }
+                  }}
+                  initial="open"
+                //   animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                //   transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                >
+                  {faq.answer}
+                </Answer>
+              </FAQ>
+            ))}
+          </FAQSContainer>
+        </Column>
       </ContentWithPaddingXl>
-    </PrimaryBackgroundContainer>
+      <DecoratorBlob1/>
+      <DecoratorBlob2 />
+    </Container>
   );
 };
