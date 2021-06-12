c803d61f73855275a1f6e9768f26490e6602734c
Jagan <jaganraajan@gmail.com>
2021-06-12 19:54:19 -0400

    added card for new members

---
+++ src/components/cards/ProfileThreeColGrid3.js
@@ -0,0 +1,232 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro";
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings";
+import {SectionDescription} from "components/misc/Typography";
+import { ReactComponent as TwitterIcon} from "images/twitter-icon.svg";
+import { ReactComponent as LinkedinIcon} from "images/linkedin-icon.svg";
+import { ReactComponent as GithubIcon } from "images/github-icon.svg";
+// import Surya from "images/surya.jpg";
+
+const HeadingContainer = tw.div``
+const Heading = tw(SectionHeading)``
+const Subheading = tw(SubheadingBase)`text-center mb-3`
+const Description = tw(SectionDescription)`mx-auto text-center`
+
+const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
+const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`
+const CardImage = styled.div`
+  ${props => css`background: url("${props.imageSrc}") no-repeat;`}
+  ${tw`w-64 h-64 bg-contain bg-center rounded`}
+`
+const CardContent = styled.div`
+  ${tw`flex flex-col items-center mt-6`}
+  .position {
+    ${tw`uppercase font-bold tracking-widest text-xs text-primary-500`}
+  }
+  .name {
+    ${tw`mt-1 text-xl font-medium text-gray-900`}
+  9
+`
+
+const CardLinks = styled.div`
+  ${tw`mt-6 flex`}
+  .link {
+    ${tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
+    .icon {
+      ${tw`fill-current w-6 h-6`}
+    }
+  }
+`
+
+export default ({
+  heading = "The 38th Committee of Daffodils Toastmasters Club",
+  subheading = "The new team",
+  description = "New leaders who will uphold the legacy of our club for the term July to December 2021" ,
+  cards = [
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/kishan.jpeg?alt=media&token=52d50a74-c8c4-4e1b-bc06-edd21a5bef61",
+      position: "President",
+      name: "TM Kishan",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/manoj.jfif?alt=media&token=9377c8d9-4966-4bf3-acc1-d592bd7ffb6b",
+      position: "Vice-president education",
+      name: "TM Manoj",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/ekta2.jpg?alt=media&token=e3df4a21-eb46-422f-9754-b7edc22408e4",
+      position: "Vice-president membership",
+      name: "TM Ekta",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/manu.jfif?alt=media&token=1c7e07b6-a64f-45d7-af27-4d327ba57c97",
+      position: "Vice-president Public Relations",
+      name: "TM Manu",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/harini2.jpg?alt=media&token=043194c0-5a44-4087-b07a-77a4405f65d2",
+      position: "Secretary",
+      name: "TM Harini",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/sona2.jpg?alt=media&token=937e1e1c-846f-4a8a-abf9-18e6ac3d5836",
+      position: "Treasurer",
+      name: "TM Sona",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/anup2.jpg?alt=media&token=df6abfd5-eb9e-43a1-b634-8bd45c4d6070",
+      position: "Sargeant-At-Arms",
+      name: "TM Anup",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://firebasestorage.googleapis.com/v0/b/virtual-influence.appspot.com/o/aashish.jpg?alt=media&token=d9f0ae1c-aaea-4e02-a8c2-bd9c637cedd6",
+      position: "Immediate Past President",
+      name: "TM Aashish",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+  ]
+}) => {
+  return (
+    <Container>
+      <ContentWithPaddingXl>
+        <HeadingContainer>
+          {subheading && <Subheading>{subheading}</Subheading>}
+          {heading && <Heading>{heading}</Heading> }
+          {description && <Description>{description}</Description> }
+        </HeadingContainer>
+        <Cards>
+          {cards.map((card, index) => (
+            <Card key={index}>
+              <CardImage imageSrc={card.imageSrc} />
+              <CardContent>
+                <span className="position">{card.position}</span>
+                <span className="name">{card.name}</span>
+                {/* <CardLinks>
+                  {card.links.map((link, linkIndex) => (
+                    <a key={linkIndex} className="link" href={link.url}>
+                      <link.icon className="icon" />
+                    </a>
+                  ))}
+                </CardLinks> */}
+              </CardContent>
+            </Card>
+          ))}
+        </Cards>
+      </ContentWithPaddingXl>
+    </Container>
+  );
+};
